102cd156c8f494be147cbcd4452f181860a18552
Leejinuk <leejin7900@gmail.com>
2024-08-08 18:09:36 +0900

    Feat: Variable Study

---
+++ Variable/ex 04-02/SaveMultipleValues.js
@@ -0,0 +1,22 @@
+// 변수는 하나의 값을 저장하기 위한 수단이다.
+var userId = 1;
+var userName = "Lee";
+
+// 갯체나 배열 같은 자료구조를 여러 개의 값을 하나로 그룹화해서 하나의 값처럼 사용할 수 있다.
+var user = { id: 1, name: "Lee" };
+
+var users = [
+  {
+    id: 1,
+    name: "Lee",
+  },
+  {
+    id: 2,
+    name: "Kim",
+  },
+];
+
+console.log(userId); // 1
+console.log(userName); // Lee
+console.log(user);
+console.log(users);
---
+++ Variable/ex 04-18/Convention.js
@@ -0,0 +1,11 @@
+// 카멜 케이스 (camelCase)
+var firstName;
+
+// 스네이크 케이스 (snake-case)
+var first_name;
+
+// 파스칼 케이스 (PascalCase)
+var FirstName;
+
+// 변수나 함수의 이름에는 카멜케이스
+// 생성자 함수, 클래스의 이름에는 파스칼케이스
