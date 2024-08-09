c5db38ea28a50ec3d7c2e0b58d8f258b73d3d7e9
Leejinuk <leejin7900@gmail.com>
2024-08-09 21:53:30 +0900

    Feat: Literal.js

---
+++ Expression&Statement/ex 05-03/Literal.js
@@ -0,0 +1,44 @@
+// 정수 리터럴
+100;
+
+// 부동소수점 리터럴
+10.5;
+
+// 2진수 리터럴
+0b01000001;
+
+// 8진수 리터럴
+0o101;
+
+// 16진수 리터럴
+0x41;
+
+// 문자열 리터럴
+("Hello");
+
+// 불리언 리터럴
+true;
+false;
+
+// null 리터럴
+null;
+
+// undefined 리터럴
+undefined;
+
+// 객체 리터럴
+{
+  name: "Lee";
+  address: "Seoul";
+}
+
+// 배열 리터럴
+[1, 2, 3];
+
+// 함수 리터럴
+const myFunction = function () {};
+
+myFunction();
+
+// 정규 표현식 리터럴
+/[A-Z]+/g;
